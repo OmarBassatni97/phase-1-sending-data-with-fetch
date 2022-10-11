05ff66ff7e999de33df4ed7aa93d1723eeb3a938
omarbassatni <omarbassatni@gmail.com>
2022-10-11 17:19:15 +0300

    finished

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your code here
+const body = document.querySelector('body')
+const p = document.createElement('p')
+body.appendChild(p)
+const submitData = (name, email) => {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: 'application/json'
+        },
+        body: JSON.stringify({
+            name: name,
+            email: email
+        })
+
+    }).then(response => response.json())
+    .then(data => p.innerHTML = data.id)
+    .catch(error => p.innerHTML = error)
+}
+
+ 
